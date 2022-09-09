a752703534ef0b190fc35f00801b93cfbc7cb906
GRCursino <guiiix93@gmail.com>
2022-09-08 23:19:35 -0300

    Header updated

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../components/Header";
+import { Header } from "../../components/Header";
+import { LayoutContainer } from "./styles";
 
 export function DefaultLayout() {
   return(
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet /> {/* Espaço que vai ser inserido contéudo, exemplo: outros componentes, códigos diversos, etc*/}
-    </div>
+    </LayoutContainer>
   );
 }
